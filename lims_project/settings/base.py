981812994157d5d5b61195f3d46e664b86603c9e
IzikN <isnevisaac@gmail.com>
2025-09-18 09:18:34 +0100

    supabase

--- lims_project/settings/base.py
+++ lims_project/settings/base.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -134,10 +135,7 @@ WSGI_APPLICATION = 'lims_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config("DATABASE_URL"))
 }
 
 
